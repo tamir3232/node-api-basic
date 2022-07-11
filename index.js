5174382a810aad319f4b8ed3c61b14a0cd89642b
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2022-07-11 21:12:47 +0700

    add docs api

--- index.js
+++ index.js
@@ -31,20 +31,53 @@ app.get('/users', (req, res, next) => {
         return res.status(404).json({
             message: 'user tidak ditemukan'
         })
-    } else {
-        // kalo ada data, return datanya
-        return res.status(200).json({
-            message: 'user ditemukan',
-            data: users
-        })
     }
+
+    // kalo ada data, return datanya
+    return res.status(200).json({
+        message: 'user ditemukan',
+        data: users
+    })
 })
 
 // create new user
+app.post('/users', (req, res, next) => {
+    // read file users.json
+
+    // tambah data baru ke array of user
+
+    // konversi data array/object ke string
+
+    // ditulis ulang data baru ke file users.json
+
+      // dimunculin respon bahwa data berhasil dibuat
+})
 
 // update existing user
+app.patch('/users/:id', (req, res, next) => {
+    // nyari data dengan id tertentu ada atau engga
+
+    // kalo ngga ada respon data not found
+
+    // kalo ada, kita update data dari request
+
+    // save ulang data ke users.json
+
+      // dimunculin respon bahwa data berhasil diupdate
+})
 
 // delete user
+app.delete('/users/:id', (req, res, next) => {
+    // cari datanya by id
+
+    // datanya ga ketemu => respon not found
+
+    // kalo datanya ketemu baru kita hapus data tersebut
+
+    // simpan data ke users.json
+
+    // dimunculin respon bahwa data berhasil dihapus
+})
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
